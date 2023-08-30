2a788d15e983fa4cb6cb2cc1389abf0dcfe3b90b
Erkan <erkan_uzunca@hotmail.com>
2023-08-30 23:14:11 +0300

    add TaskDto

---
+++ TodoList/src/main/java/com/hamitmizrak/business/dto/TaskDto.java
@@ -0,0 +1,32 @@
+package com.hamitmizrak.business.dto;
+
+import com.hamitmizrak.auditing.AuditingAwareBaseDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import java.util.Date;
+import java.io.Serializable;
+
+// LOMBOK
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Log4j2
+// Validation
+public class TaskDto extends AuditingAwareBaseDto implements Serializable {
+
+    // serileştirme
+    public static final Long serialVersionUID=1L;
+
+    private Long taskId;
+    private String taskTitle;
+    private boolean taskCompleted;
+    private Date taskDate=new Date(System.currentTimeMillis());
+
+
+
+} //end class
+
